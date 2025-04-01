ca5719413e0be4c31dd01d4e4f02501f36f3ccc8
Erick Cocoletzi <erickalbertosantillancocoletzi@gmail.com>
2025-04-01 00:03:07 -0600

    implemnetacion de list y array list con html

---
+++ src/org/ericksantillan/pooclasesabst/form/elemtos/SelectForm.java
@@ -0,0 +1,49 @@
+package org.ericksantillan.pooclasesabst.form.elemtos;
+
+import org.ericksantillan.pooclasesabst.form.elemtos.select.Opcion;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelectForm extends ElementosForm{
+    private List<Opcion> opciones;
+    //constructor
+
+    public SelectForm(String nombre) {
+        super(nombre);
+        this.opciones=new ArrayList<Opcion>();
+    }
+
+    public SelectForm(String nombre, List<Opcion> opciones) {
+        super(nombre);
+        this.opciones = opciones;
+    }
+
+    //getter and setter
+    public SelectForm addOpcion(Opcion opcion){
+        this.opciones.add(opcion);
+        return this;
+    }
+    //metodos
+    @Override
+    public String dibujarhtml() {
+        StringBuilder sb=new StringBuilder("<select ");
+        sb.append("name='")
+                .append(this.nombre)
+                .append("'>");
+
+        for (Opcion opcion: this.opciones){
+            sb.append("\n<option value='")
+                    .append(opcion.getValor())
+                    .append("'");
+            if(opcion.isSelected()){
+                sb.append(" selected");
+            }
+            sb.append(">")
+                    .append(opcion.getNombre())
+                    .append("</option>");
+
+        }
+        sb.append("</select");
+        return sb.toString();
+    }
+}
